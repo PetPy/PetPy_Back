bc083eb893ce09ca079e361c19fef2f7f08c3423
hyeonuk-kor <mrseos@naver.com>
2022-09-25 22:52:02 +0900

    회원가입 페이지 작성

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ app.listen(port, function () {
 	console.log("listening on " + port);
 });
 
-app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/index.html");
+function isLogin(req, res, next) {
+	if (req.user) next();
+	else res.render("login.ejs");
+}
+
+app.get("/", isLogin, (req, res) => {
+	res.render("home.ejs");
 });
 
-app.get("/login", (req, res) => {
-	res.render("login.ejs");
+app.get("/register", (req, res) => {
+	res.render("register.ejs");
 });
